cfa9d690ce5e558acf9089bb2d95c03e96e7a0ef
Fei Zhang <feizhang@codeaurora.org>
2014-04-29 12:36:30 +0800

    QCamera2: use cam_lock when camera open
    In dual Camera use case, when one camera opened and in preview status,
    if second camera enter into open operation, in current code,
    camera_open() will hold g_intf_lock until open finished. So during
    second open, the operation needed by first camera will be blocked
    when try to get g_intf_lock (example is preview frame bufDone).
    
    So we need use cam_lock instead when opening, and not block other
    camera obj's operation.
    
    Change-Id: Ie6e4202be64735ce24a5a35b02be7eaf22c5d69d

--- camera/QCamera2/stack/mm-camera-interface/src/mm_camera.c
+++ camera/QCamera2/stack/mm-camera-interface/src/mm_camera.c
@@ -328,9 +328,10 @@ int32_t mm_camera_open(mm_camera_obj_t *my_obj)
                                  MM_CAMERA_POLL_TYPE_EVT);
     mm_camera_evt_sub(my_obj, TRUE);
 
+    /* unlock cam_lock, we need release global intf_lock in camera_open(),
+     * in order not block operation of other Camera in dual camera use case.*/
+    pthread_mutex_unlock(&my_obj->cam_lock);
     CDBG("%s:  end (rc = %d)\n", __func__, rc);
-    /* we do not need to unlock cam_lock here before return
-     * because for open, it's done within intf_lock */
     return rc;
 
 on_error:
@@ -343,8 +344,9 @@ on_error:
        my_obj->ds_fd = 0;
     }
 
-    /* we do not need to unlock cam_lock here before return
-     * because for open, it's done within intf_lock */
+    /* unlock cam_lock, we need release global intf_lock in camera_open(),
+     * in order not block operation of other Camera in dual camera use case.*/
+    pthread_mutex_unlock(&my_obj->cam_lock);
     return rc;
 }
 
