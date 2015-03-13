0dda16f145e33da294136523d0c75e4aaaa6ef4b
Edward Wang <ewang@cyngn.com>
2015-03-13 15:15:41 -0700

    rendang: set bluetooth default name
    Change-Id: I9366c545362baa99cae80f8f23511fc65f2d0230

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "G3"
+#define BTM_DEF_LOCAL_NAME   "Smartfren Andromax G36C1H"
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
