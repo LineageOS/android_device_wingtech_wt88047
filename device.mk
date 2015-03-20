b5a0407fb6a824f55b47997697ee55265485fc66
Howard M. Harte <hharte@magicandroidapps.com>
2015-03-19 19:51:57 -0700

    rendang: add haptic feedback for virtual keys
    Change-Id: Ib70675cdd4b9a51d11a8ce5fe2ec87354f1447be

--- device.mk
+++ device.mk
@@ -150,6 +150,7 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/msg2xxx.kl:system/usr/keylayout/msg2xxx.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl
 
 # Keystore
