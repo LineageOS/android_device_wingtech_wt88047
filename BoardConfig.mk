e766a935d59fdab77503e88dd0f33d18a3891805
Venkatraman Nerellapalli <venkataraman.nerellapalli@codeaurora.org>
2015-06-19 15:11:50 +0530

    msm8916_32: Enable CSVT for 8916_32 target
    Added new variable for CSVT in config file.
    
    Change-Id: I689c17a092a5072c99f0e23149d7999437e76cd5
    CRs-Fixed: 815598

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 DEVICE_PATH := device/wingtech/wt88047
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
+# Enables CSVT
+TARGET_USES_CSVT := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME :=
 
