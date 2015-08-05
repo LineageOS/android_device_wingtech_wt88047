554e03710fd4209a77894220661969a1070653ae
Ethan Chen <intervigil@gmail.com>
2015-08-05 13:55:01 -0700

    rendang: Move USB IDs to default properties
    Change-Id: Ibad7a6809dd0f0daffabcec006a350179c8909c1

--- device.mk
+++ device.mk
@@ -56,6 +56,17 @@ PRODUCT_PACKAGES += \
     camera.msm8916 \
     libmm-qcamera
 
+# Charger
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.usb.vid=201E \
+    ro.usb.id.charge=F006 \
+    ro.usb.id.mtp=2282 \
+    ro.usb.id.mtp_adb=2281 \
+    ro.usb.id.ptp=2284 \
+    ro.usb.id.ptp_adb=2283 \
+    ro.usb.id.ums=2286 \
+    ro.usb.id.ums_adb=2285
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8916
