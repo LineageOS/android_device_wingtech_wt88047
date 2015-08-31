ba887de7e24fd04c33b550541c6ac10f21d8da96
Lin Ma <lma@cyngn.com>
2015-08-31 11:49:04 -0700

    wt88047: Disable BLE peripheral advertiser name
    * In order to pass Bluetooth BLE advertiser and scanner test
    
    Change-Id: Ief7452f0d29fc8cb337544dca27851996023736e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -26,5 +26,5 @@
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
 #define BLE_VND_INCLUDED   TRUE
-#define BLE_PERIPHERAL_ADV_NAME  TRUE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #endif
