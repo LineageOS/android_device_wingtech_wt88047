19f004165c70f857d25f39cf6649119ad44c5300
Deepak Kundra <deepakkundra@gmail.com>
2015-09-02 13:37:39 -0700

    IMS: Fix compile errors
    Change-Id: I8185739458b14aa8998fb2b406e53102e6cc649b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,13 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 
+# Init scripts
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qti.ims.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qti.ims.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-# Init scripts
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
