ee07ceb60c9c3bfd0031a28535af1ce456028a9d
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-09-23 05:19:41 +0700

    wt88047: Bring back camera source
    From CAF LNX.LA.3.7.2_rb1
    
    Change-Id: Idd7e355e78f87b0930bee43f4ad0fa5d21038d80

--- device.mk
+++ device.mk
@@ -52,6 +52,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8916 \
+    libmm-qcamera
+
 # Enable USB OTG in storage setting
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.isUsbOtgEnabled=true
