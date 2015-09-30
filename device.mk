dd4b2809b4b0be852ca2b8848fcbf43e7b46bd31
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-09-30 21:47:56 +0700

    wt88047: Use the correct memory configuration
    Credit: sundarajan srinivasan <ssrinivasan@cyngn.com>
    
    Change-Id: Iacd9f763fbf09ce0cff87b3ee3f431d9e0dabbd4

--- device.mk
+++ device.mk
@@ -36,8 +36,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
