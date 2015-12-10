d71a044f7e5ad0146bc84396164d851437330fd6
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-12-10 15:47:25 +0700

    Revert "wt88047: Restore heaset button keylayout"
    This reverts commit 9d90e465b9c134b79af78e9c6e7e547c11086b39.

--- device.mk
+++ device.mk
@@ -122,7 +122,6 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/msm8x16-wt88047-snd-card_Button_Jack.kl:system/usr/keylayout/msm8x16-wt88047-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl
 
