ec06226c872aff692ec53b0d7fc2b9cf0f48fcd5
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-12-26 12:56:00 +0700

    Revert "Revert "wt88047: Switch to SELinux enforcing""
    This reverts commit 2e62ce96223c4217573fb53502bf01556c909b4b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,6 @@ BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Set vold to use native exfat driver
 TARGET_KERNEL_HAVE_EXFAT := true
