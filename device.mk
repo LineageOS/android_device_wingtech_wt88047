d4b11a86479d965b6283d7490bbc2764cd5825cc
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-01-10 09:40:59 +0700

    wt88047: Proper mixer config name for audio HAL
    * Follow in kernel audio device rename
    
    Change-Id: Idff29cfc51e24cabae680d4e6656d6adc244e86b

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/QRD_Speaker_cal.acdb:system/etc/acdbdata/QRD/QRD_Speaker_cal.acdb \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_qrd_skui.xml
 
 # Camera
 PRODUCT_PACKAGES += \
