e74bf6fcb3e05190f1a297944d62b9e81b5c17e3
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-03-13 07:20:45 +0700

    wt88047: init: More friendly to non cm build
    Change-Id: I5aaa13c6c12aa0409d5b61d0bfb4045242b94703

--- init/init_wt88047.cpp
+++ init/init_wt88047.cpp
@@ -197,8 +197,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     UNUSED(msm_ver);
     UNUSED(board_type);
 
-    rc = property_get("ro.cm.device", device);
-    if (!rc || !ISMATCH(device, "wt88047"))
+    rc = property_get("ro.product.name", device);
+    if (!rc || (strstr(device, "wt88047") == NULL))
         return;
 
     import_kernel_cmdline(0, import_kernel_nv);
