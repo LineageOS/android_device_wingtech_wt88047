0b7e02076d06066e5193aa154eea1b9cbabb8d3d
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-04-30 15:40:12 -0700

    wt88047: Use own kernel
    Change-Id: I74e12561a5fc9ae83bd0cd400b36500b0df499f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_BOOTLOADER_BOARD_NAME :=
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1
-TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
+TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
 
 # CPU
