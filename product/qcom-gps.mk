4573af46df6e4349350c5860043962abae61d7c3
Ethan Chen <intervigil@gmail.com>
2016-08-21 12:38:21 +0700

    wt88047: Enable QC location service
    Change-Id: I3fcb5b9a2847641c09870f6ad41ea1525ad47426

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -12,3 +12,9 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=1 \
+    persist.loc.nlp_name=com.qualcomm.location \
+    ro.gps.agps_provider=1
