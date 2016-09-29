c0fdac2a3ea6e47b784fa8812a838781c319d47e
Prema Chand Alugu <premaca@gmail.com>
2016-09-30 04:10:47 +0530

    wt88047: Build Telephony and IMS ext
    Change-Id: I9b77007d70bc4b9e0fd78e8c3dd4dbdac32e0626

--- device.mk
+++ device.mk
@@ -286,6 +286,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.target=dpm1 \
     persist.data.qmi.adb_logmask=0
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext ims-ext-common
+PRODUCT_BOOT_JARS += telephony-ext
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
