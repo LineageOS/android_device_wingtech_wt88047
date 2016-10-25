bb2e55d64d334b96f7c512e7837e7ab15c8ff39b
Suman Mukherjee <sumam@codeaurora.org>
2016-10-25 16:14:33 +0530

    QCamera2: Fix memory leak when fail to open camera device.
    Issue:
    When camera open fails, param heap buffer which was allocated
    in a deferred thread will not be cleaned up properly because
    of improper flag check in parameters deinit().
    Fix:
    Since param heap buffer is allocated asynchronously, move the
    initialization flag check after deallocating param heap.
    CRs-Fixed: 1070448
    Change-Id: I026f307c5b7501d3607c64c6b36d41eddaf0349a

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -5234,6 +5234,12 @@ TRANS_INIT_DONE:
  *==========================================================================*/
 void QCameraParameters::deinit()
 {
+    if (NULL != m_pParamHeap) {
+        m_pParamHeap->deallocate();
+        delete m_pParamHeap;
+        m_pParamHeap = NULL;
+        m_pParamBuf = NULL;
+    }
     if (!m_bInited) {
         return;
     }
@@ -5249,12 +5255,6 @@ void QCameraParameters::deinit()
         m_pCamOpsTbl = NULL;
     }
     m_pCapability = NULL;
-    if (NULL != m_pParamHeap) {
-        m_pParamHeap->deallocate();
-        delete m_pParamHeap;
-        m_pParamHeap = NULL;
-        m_pParamBuf = NULL;
-    }
 
     m_AdjustFPS = NULL;
 
