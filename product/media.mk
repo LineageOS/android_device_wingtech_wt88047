fdc39bdf29a7972d578567dc1e4b4d2ae11b9f27
nicknitewolf <nickradiantwtf@gmail.com>
2016-11-10 15:42:13 +0000

    wt88047: Enable legacy HAL1 HAX
    Change-Id: Ie907ae9aa4b4c8ea7e02a0cd5d82b3f83b9be183

--- product/media.mk
+++ product/media.mk
@@ -6,3 +6,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+
+# Force legacy HAL1 HAX
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
