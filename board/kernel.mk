f9730580aea2a5b90d4bb481aafefb501b251ee6
nicknitewolf <nickradiantwtf@gmail.com>
2016-11-25 11:12:45 +0800

    wt88047: set permissive for now
    Change-Id: I8b0c821716ca60fadea5fe707c0ad6fff9529d32

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,6 +4,7 @@ TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
 
 BOARD_KERNEL_CMDLINE += \
     sched_enable_hmp=1 \
-    phy-msm-usb.floated_charger_enable=1
+    phy-msm-usb.floated_charger_enable=1 \
+    androidboot.selinux=permissive
 
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
