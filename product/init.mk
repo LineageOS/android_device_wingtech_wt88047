0b3a5002625363b79162445df0e773ac949cdb78
nicknitewolf <nickradiantwtf@gmail.com>
2016-12-04 10:25:38 +0000

    wt88047: replace mpdecision with perfd
    By doing so, we do not need a custom power init for wt88047 and we can commonize to use msm8916-common power init.
    
    Change-Id: Ifdb2536b5b10671c88b82968fcf46c6c5d0ebfbb

--- product/init.mk
+++ product/init.mk
@@ -2,6 +2,5 @@
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc \
-    init.target.power.rc \
     init.qcom.usb.rc \
     memcheck
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,12 +40,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.power.rc
-LOCAL_OVERRIDES_PACKAGES := init.qcom.power.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.target.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
