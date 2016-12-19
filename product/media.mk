cba26b3c57ad1f596cff9cc53facd126b1046b73
NeoDarkness <aldi.45ckr@gmail.com>
2016-12-18 18:45:13 -0800

    wt88047: device tree clean up
    Change-Id: Ic6e8489c1f5d5c61c0a43474107aeaff5361baca

--- product/media.mk
+++ product/media.mk
@@ -6,8 +6,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
-
-# Force legacy HAL1 HAX
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -13,14 +13,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
-# Disable the thumbnail encoding in order to reduce the power
+# Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.tn.disable=1
-
-# System property for PIP support
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.pip.support=0
-
-# Disable all camera debug
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.hal.debug.mask=0
+    persist.camera.tn.disable=1 \
+    persist.camera.pip.support=0 \
+    persist.camera.hal.debug.mask=0 \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
