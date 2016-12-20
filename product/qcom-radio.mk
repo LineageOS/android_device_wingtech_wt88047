53fb240485df64783cb73d1677cd2f65d39f1970
NeoDarkness <aldi.45ckr@gmail.com>
2016-12-19 23:08:41 -0800

    wt88047: add default network
    Change-Id: Idaeebe383c99c583db67754ab2593b0b35bab0f0

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -8,6 +8,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.ecc_hard_1=112,911,110,122,119,120,000,118 \
     persist.radio.ecc_hard_count=1 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
+    ro.telephony.default_network=9,1 \
     ril.subscription.types=RUIM \
     persist.radio.rat_on=combine \
     ro.ril.multi_rat_capable=true
