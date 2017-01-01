7b3e2b49ba9fc65a1a31898257e6e371acd74e45
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-01 13:15:35 +0000

    wt88047: Clean up
    Change-Id: I883ea83fd46dbf1f14e22435248990830d1170ae

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,4 @@
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := wt88047_defconfig
-
-BOARD_KERNEL_CMDLINE += \
-    sched_enable_hmp=1 \
-    phy-msm-usb.floated_charger_enable=1
+BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
