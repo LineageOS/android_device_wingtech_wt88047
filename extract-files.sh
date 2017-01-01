debbff8194e3da8a251148e3d7087564c6889f2c
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-01 13:15:35 +0000

    wt88047: Clean up
    -remove unused file
    -fix indent space
    -remove double flag
    -move some flag to correct location
    
    Change-Id: I883ea83fd46dbf1f14e22435248990830d1170ae

--- extract-files.sh
+++ extract-files.sh
@@ -34,19 +34,19 @@ fi
 . "$HELPER"
 
 if [ $# -eq 0 ]; then
-  SRC=adb
+    SRC=adb
 else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
+    if [ $# -eq 1 ]; then
+        SRC=$1
+    else
+        echo "$0: bad number of arguments"
+        echo ""
+        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+        echo ""
+        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+        echo "the device using adb pull."
+        exit 1
+    fi
 fi
 
 "$CM_ROOT"/device/qcom/common/extractors/extract-files.sh "$SRC" msm8916 graphics
