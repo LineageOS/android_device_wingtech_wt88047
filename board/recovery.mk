504027efae947e2eb5ea4fd7208c486fcd7563c9
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-01 13:15:35 +0000

    wt88047: Clean up
     * remove unused file
     * fix indent space
     * remove double flag
     * move some flag to correct location
    
    Change-Id: I883ea83fd46dbf1f14e22435248990830d1170ae

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,6 +1,5 @@
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
-
-# Filesystem
-TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+BOARD_NO_SECURE_DISCARD := true
+
