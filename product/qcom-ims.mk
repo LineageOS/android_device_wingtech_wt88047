169bd5d5f381659e70a26c58b56a385378e29f03
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-04 08:57:12 +0000

    wt88047: ims: stop building prebuilt items
     * IMS Prebuilts are already included in vendor repo.
       Stop trying to rebuild what is there.
    
    Change-Id: If564b868dee21e7f746de09db4360c866223ced4

--- product/qcom-ims.mk
+++ product/qcom-ims.mk
@@ -1,22 +1,7 @@
 # IMS
-PRODUCT_PACKAGES += \
-    ims \
-    imscmlibrary \
-    imssettings \
-    init.qti.ims.sh
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/ims/imscm.xml:system/etc/permissions/imscm.xml \
-    $(LOCAL_PATH)/configs/ims/ims.xml:system/etc/permissions/ims.xml \
-    $(LOCAL_PATH)/configs/ims/qti_permissions.xml:system/etc/permissions/qti_permissions.xml
-
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.jbims=1 \
     persist.radio.ROTATION_ENABLE=1 \
     persist.radio.VT_ENABLE=1 \
     persist.radio.VT_HYBRID_ENABLE=1 \
     persist.radio.VT_USE_MDM_TIME=0
-
-# Telephony-ext
-PRODUCT_PACKAGES += telephony-ext ims-ext-common
-PRODUCT_BOOT_JARS += telephony-ext
