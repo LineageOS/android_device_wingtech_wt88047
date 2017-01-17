ff4807bbb2afa66d5f0186f9d055248068f6367e
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-17 10:40:36 +0000

    wt88047: audio: Disable fluence mode
    Change-Id: I195e9af29d447e524b637acd62eda121ebec9e1d

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -21,7 +21,7 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=fluence \
+    ro.qc.sdk.audio.fluencetype=none \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=true
