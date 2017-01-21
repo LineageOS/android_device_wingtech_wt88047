2ef995480f437e0da2333fd6b0b0699ce509e4b7
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-21 04:28:27 +0000

    wt88047: audio: Disable fluence for speaker
     * Do not use fluence for speaker
    
    Change-Id: Ifd83ea3c1e73bbe7871ec67833ee01fd3f67aeef

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -24,4 +24,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=true
+    persist.audio.fluence.speaker=false
