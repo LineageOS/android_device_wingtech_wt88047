57c079525149b985f910d91d05b3926ff2433b9d
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-21 04:27:48 +0000

    Revert "wt88047: audio: Disable fluence mode"
    This reverts commit ff4807bbb2afa66d5f0186f9d055248068f6367e.
    
    Change-Id: I0db718f44cc336cae983304831355a74bd1caf1c

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -21,7 +21,7 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=none \
+    ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=true
