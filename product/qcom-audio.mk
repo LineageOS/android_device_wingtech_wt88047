5fc59ba9cc09f96538a045b5a02b9bd21be12988
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-26 22:14:16 +0000

    wt88047: audio: mixer_paths -> mixer_paths_qrd_skui
    Change-Id: I3a49c652b79e8566d0aa0f147372edffd66d2a5b

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -9,7 +9,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/QRD_Speaker_cal.acdb:system/etc/acdbdata/QRD/QRD_Speaker_cal.acdb \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_qrd_skui.xml
+    $(LOCAL_PATH)/audio/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml
 
 # On boot audio lineout config
 PRODUCT_PACKAGES += \
