de7e69ac86cd1f736710fff75148e6fd65ebee79
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-29 03:49:09 +0000

    wt88047: Remove deprecated ro.ril.multi_rat_capable
     * It's true by default.
    
    Change-Id: I966cce3253a78f92eb9bcbceb69fffe896dc890a

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -7,5 +7,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.ecc_hard_count=1 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
     ril.subscription.types=RUIM \
-    persist.radio.rat_on=combine \
-    ro.ril.multi_rat_capable=true
+    persist.radio.rat_on=combine
