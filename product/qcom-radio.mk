14ea3e0e2bb5d76c2de78cfc3a0306460af85016
NeoDarkness <aldi.45ckr@gmail.com>
2017-01-29 03:49:09 +0000

    wt88047: Remove deprecated radio props
     * Get rid of ro.ril.multi_rat_capable
       and persist.radio.rat_on
    
    Change-Id: I966cce3253a78f92eb9bcbceb69fffe896dc890a

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -9,6 +9,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.ecc_hard_1=112,911,110,122,119,120,000,118 \
     persist.radio.ecc_hard_count=1 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
-    ril.subscription.types=RUIM \
-    persist.radio.rat_on=combine \
-    ro.ril.multi_rat_capable=true
+    ril.subscription.types=RUIM
