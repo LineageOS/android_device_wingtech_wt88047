205995d202733c0aec9a5ad15949f7e226e3b9e1
nicknitewolf <nicknitewolf@gmail.com>
2017-02-26 23:43:04 +0800

    wt88047: Initial 64Bit Support
    Change-Id: Ib6660914046dedd09f6aa81e2e5fe2c9be13cd56

--- full_wt88047.mk
+++ full_wt88047.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +16,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from wt88047 device
