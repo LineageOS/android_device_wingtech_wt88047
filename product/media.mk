16f540e5ad46ceb9ab0d2510a2af25da167633cc
Arvin Quilao <arquilao@gmail.com>
2017-03-02 03:50:42 +0000

    wt88047: Switch to common media_codecs_performance.xml
    Change-Id: Iffd7d32112bd14074d3edaf98904903b90ef6856

--- product/media.mk
+++ product/media.mk
@@ -1,4 +1,3 @@
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
