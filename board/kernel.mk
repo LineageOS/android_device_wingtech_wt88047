148d14c1271eecfa922f0fee7d9e439462f80140
Louis Popi <theh2o64@gmail.com>
2017-03-18 09:53:18 +0100

    DO NOT MERGE: wt88047: Switch to cyanogen/msm8916 kernel
    Change-Id: Iff0f4ba89fdafc65788b038ff6cd072591f8e109

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,4 @@
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
+TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 TARGET_KERNEL_CONFIG := wt88047_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
