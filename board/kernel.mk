f110833f3f6ad8736570d8af63b6a84ba31b3960
nicknitewolf <nicknitewolf@lineageos.org>
2017-05-17 18:49:30 +0800

    wt88047: Enable SVELTE memory configuration
    * Enable SVELTE memory configuration
    
    Change-Id: I9f09ad30b535698a56cf80d1ce9cc175c5f41269

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,3 +2,6 @@
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := wt88047_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
+
+# Bionic
+MALLOC_SVELTE := true
