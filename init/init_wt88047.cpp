0689c3e61318e2edc66a2ab1d0919a38bc92f97c
LuK1337 <priv.luk@gmail.com>
2017-06-16 23:05:33 +0800

    wt88047: Kill TARGET_UNIFIED_DEVICE flag
    * Use PRODUCT_SYSTEM_PROPERTY_BLACKLIST instead.
    
    Change-Id: I18608e1a445ffcea72fc5a31b8c949c31ae96a44

--- init/init_wt88047.cpp
+++ init/init_wt88047.cpp
@@ -80,72 +80,72 @@ void init_target_properties()
 
     /* S88047E1 */
     if (buf.find("S88047E1") != std::string::npos) {
-        property_set("ro.build.product", "HM2014817");
-        property_set("ro.product.device", "HM2014817");
-        property_set("ro.product.model", "2014817");
-        property_set("ro.product.name", "2014817");
-        property_set("ro.telephony.default_network", "9,1");
+        property_override("ro.build.product", "HM2014817");
+        property_override("ro.product.device", "HM2014817");
+        property_override("ro.product.model", "2014817");
+        property_override("ro.product.name", "2014817");
+        property_override("ro.telephony.default_network", "9,1");
     }
     /* S88047D1 */
     else if (buf.find("S88047D1") != std::string::npos) {
-        property_set("ro.build.product", "HM2014819");
-        property_set("ro.product.device", "HM2014819");
-        property_set("ro.product.model", "2014819");
-        property_set("ro.product.name", "2014819");
+        property_override("ro.build.product", "HM2014819");
+        property_override("ro.product.device", "HM2014819");
+        property_override("ro.product.model", "2014819");
+        property_override("ro.product.name", "2014819");
         property_set("ro.telephony.default_network", "9,1");
     }
     /* S88047C1 */
     else if (buf.find("S88047C1") != std::string::npos) {
-        property_set("ro.build.product", "HM2014818");
-        property_set("ro.product.device", "HM2014818");
-        property_set("ro.product.model", "2014818");
-        property_set("ro.product.name", "2014818");
+        property_override("ro.build.product", "HM2014818");
+        property_override("ro.product.device", "HM2014818");
+        property_override("ro.product.model", "2014818");
+        property_override("ro.product.name", "2014818");
         property_set("ro.telephony.default_network", "9,1");
         property_set("persist.dbg.volte_avail_ovr", "1");
         property_set("persist.dbg.vt_avail_ovr", "1");
     }
     /* S88047B2 */
     else if (buf.find("S88047B2") != std::string::npos) {
-        property_set("ro.build.product", "HM2014821");
-        property_set("ro.product.device", "HM2014821");
-        property_set("ro.product.model", "2014821");
-        property_set("ro.product.name", "2014821");
+        property_override("ro.build.product", "HM2014821");
+        property_override("ro.product.device", "HM2014821");
+        property_override("ro.product.model", "2014821");
+        property_override("ro.product.name", "2014821");
         property_set("ro.telephony.default_network", "22,1");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("persist.radio.sglte.eons_domain", "ps");
     }
     /* S88047B1 */
     else if (buf.find("S88047B1") != std::string::npos) {
-        property_set("ro.build.product", "HM2014812");
-        property_set("ro.product.device", "HM2014812");
-        property_set("ro.product.model", "2014812");
-        property_set("ro.product.name", "2014812");
+        property_override("ro.build.product", "HM2014812");
+        property_override("ro.product.device", "HM2014812");
+        property_override("ro.product.model", "2014812");
+        property_override("ro.product.name", "2014812");
         property_set("ro.telephony.default_network", "22,1");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("persist.radio.sglte.eons_domain", "ps");
     }
     /* S86047A1 and S86047A1_CD */
     else if (buf.find("S86047A1") != std::string::npos) {
-        property_set("ro.build.product", "HM2014813");
-        property_set("ro.product.device", "HM2014813");
-        property_set("ro.product.model", "2014813");
-        property_set("ro.product.name", "2014813");
+        property_override("ro.build.product", "HM2014813");
+        property_override("ro.product.device", "HM2014813");
+        property_override("ro.product.model", "2014813");
+        property_override("ro.product.name", "2014813");
         property_set("ro.telephony.default_network", "9,1");
     }
     /* S86047A2 and S86047A2_CD */
     else if (buf.find("S86047A2") != std::string::npos) {
-        property_set("ro.build.product", "HM2014112");
-        property_set("ro.product.device", "HM2014112");
-        property_set("ro.product.model", "2014112");
-        property_set("ro.product.name", "2014112");
+        property_override("ro.build.product", "HM2014112");
+        property_override("ro.product.device", "HM2014112");
+        property_override("ro.product.model", "2014112");
+        property_override("ro.product.name", "2014112");
         property_set("ro.telephony.default_network", "9,1");
     }
     /* S88047A2 and S88047A1 */
     else {
-        property_set("ro.build.product", "HM2014811");
-        property_set("ro.product.device", "HM2014811");
-        property_set("ro.product.model", "2014811");
-        property_set("ro.product.name", "2014811");
+        property_override("ro.build.product", "HM2014811");
+        property_override("ro.product.device", "HM2014811");
+        property_override("ro.product.model", "2014811");
+        property_override("ro.product.name", "2014811");
         property_set("ro.telephony.default_network", "9,1");
     }
 
