7eb60ee9e00984bfa33456aa5e7ea77ab7f89bfe
nicknitewolf <nicknitewolf@lineageos.org>
2017-06-16 18:09:15 +0800

    wt88047: Move build fingerprint out of device init
    * Set build fingerprint while building not on boot
    
    Change-Id: I5442fb01262d029a2d3a39a25283ab23625a24f6

--- lineage.mk
+++ lineage.mk
@@ -28,3 +28,8 @@ BOARD_VENDOR := wingtech
 PRODUCT_DEVICE := wt88047
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Xiaomi/wt88047/wt88047:5.1.1/LMY47V/V8.1.3.0.LHJMIDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="wt88047-user 5.1.1 LMY47V V8.1.3.0.LHJMIDL release-keys"
