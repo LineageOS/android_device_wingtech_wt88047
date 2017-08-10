6e623d1e6b493a97e8af7754ce80c16463a20a50
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-10 11:22:10 +0800

    wt88047: Update extract scripts to latest templates
     * Add support for blob section exctraction
     * Cleanup
    
    Change-Id: Idd66f3263016b19b60ff1593242a5d4c3973126c

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,13 +18,12 @@
 
 set -e
 
-export INITIAL_COPYRIGHT_YEAR=2015
-
-# Required!
 DEVICE=wt88047
 VENDOR=wingtech
 
-# Load extractutils and do some sanity checks
+INITIAL_COPYRIGHT_YEAR=2015
+
+# Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
@@ -42,22 +42,7 @@ setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 # Copyright headers and guards
 write_headers
 
-# The standard blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
 
-# Qualcomm BSP blobs - we put a conditional around here
-# in case the BSP is actually being built
-printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$PRODUCTMK"
-printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$ANDROIDMK"
-
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt
-
-echo "endif" >> "$PRODUCTMK"
-
-cat << EOF >> "$ANDROIDMK"
-endif
-
-EOF
-
-# We are done!
+# Finish
 write_footers
