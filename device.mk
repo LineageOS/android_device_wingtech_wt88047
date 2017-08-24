eef4c92640fdf1ea74edf429b9ca16b9fef195d4
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-24 15:15:20 +0200

    wt88047: Move Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- device.mk
+++ device.mk
@@ -17,7 +17,9 @@
 $(call inherit-product, vendor/wingtech/wt88047/wt88047-vendor.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
