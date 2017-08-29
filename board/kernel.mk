eea303d6d3b7b2d615eebcaee5fbde9460775cc2
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-29 20:52:20 +0800

    wt88047: Use lineageos_wt88047_defconfig instead
     * Standardize defconfig name with other
       msm8916 qcom_kernel_common devices
    
    Change-Id: Ib2c485855f9e509a01e3e86af8d4bfffa68c22ce

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,4 @@
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
-TARGET_KERNEL_CONFIG := wt88047_defconfig
+TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
