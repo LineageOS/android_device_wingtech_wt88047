NeoDarkness/android_device_xiaomi_wt88047

wt88047: Enforce vendor tree presence
A compiled build has no chance of booting without the prebuilt
binaries, so fail the build if those are not present.

Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

BoardConfig.mk
@@ -23,4 +23,4 @@ include device/xiaomi/wt88047/board/*.mk
 TARGET_BOARD_INFO_FILE := device/xiaomi/wt88047/board-info.txt
 
 # inherit from the proprietary version
--include vendor/xiaomi/wt88047/BoardConfigVendor.mk
+include vendor/xiaomi/wt88047/BoardConfigVendor.mk

device.mk
@@ -23,4 +23,4 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
 
-$(call inherit-product-if-exists, vendor/xiaomi/wt88047/wt88047-vendor.mk)
+$(call inherit-product, vendor/xiaomi/wt88047/wt88047-vendor.mk)

LineageOS/android_device_wingtech_wt88047

BoardConfig.mk
@@ -21,4 +21,4 @@ DEVICE_PATH := device/wingtech/wt88047
 include $(DEVICE_PATH)/board/*.mk
 
 # inherit from the proprietary version
--include vendor/wingtech/wt88047/BoardConfigVendor.mk
+include vendor/wingtech/wt88047/BoardConfigVendor.mk

device.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product-if-exists, vendor/wingtech/wt88047/wt88047-vendor.mk)
+$(call inherit-product, vendor/wingtech/wt88047/wt88047-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay