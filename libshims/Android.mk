2c844ac6f42a8e0e9ebe5824b6eb607c6164d78b
Billchen1977 <billchen1977@gmail.com>
2017-09-18 17:43:51 +0530

    wt88047: libshims: Update IMS shim
    Change-Id: I8a73d669515b69cca7631c3cd46c6621c974b312

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -15,8 +15,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := MediaBuffer.c
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_SRC_FILES := MediaBuffer.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui
+LOCAL_CFLAGS := -Wno-unused-private-field
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
