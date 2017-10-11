3974f8bc62b58f884c1b4d39045be194a7309dbb
Michael Bestas <mkbestas@lineageos.org>
2017-10-11 19:46:32 +0300

    wt88047: Add kernel flags
    Change-Id: I7d26d5fb698f7cbc49b7169b2ddf5419b66b1caa

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,7 @@
 # Kernel
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
