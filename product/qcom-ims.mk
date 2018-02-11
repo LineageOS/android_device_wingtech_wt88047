dc9d3d85ad738ad1e2610cd62cd0fff52c9b2baf
Nikolas Lim <nicknitewolf@lineageos.org>
2018-02-12 00:38:31 +0800

    wt88047: ims: Initial oreo update
    Change-Id: Ibe2ed5223eaaf448d449f27f6078a6a58be28079

--- product/qcom-ims.mk
+++ product/qcom-ims.mk
@@ -1,10 +1,3 @@
 # IMS
 PRODUCT_PACKAGES += \
     libshims_ims
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.jbims=1 \
-    persist.radio.ROTATION_ENABLE=1 \
-    persist.radio.VT_ENABLE=1 \
-    persist.radio.VT_HYBRID_ENABLE=1 \
-    persist.radio.VT_USE_MDM_TIME=0
