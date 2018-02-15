5b1ae5d5163b0aa6e0617fb0bce84851eec26bff
jrior001 <jriordan001@gmail.com>
2018-02-15 08:16:00 +0000

    wt88047: Move gps configs to vendor
    Change-Id: Id38c24ff587d37ccbc49ef986ca128cb2b95cf2a

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -3,10 +3,10 @@ PRODUCT_PACKAGES += \
     gps.msm8916
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/flp.conf:system/vendor/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/vendor/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/vendor/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/vendor/etc/sap.conf
 
 # Permissions
 PRODUCT_COPY_FILES += \
