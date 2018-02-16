b25f0cd0708ec98a86747cd306479fd565630691
jrior001 <jriordan001@gmail.com>
2018-02-16 23:06:00 +0000

    wt88047: Move all blobs to vendor
    * Requires following hex edit to load configs from vendor:
    sed -i "s|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g" vendor/lib/libdsi_netctrl.so
    sed -i "s|/system/etc/data/netmgr_config.xml|/vendor/etc/data/netmgr_config.xml|g" vendor/bin/netmgrd
    * Also requires edit of qcnvitems.xml and qcrilhook.xml
      to load jars from vendor
    
    Change-Id: I89844660594c18ea905217122b507211f8f89ca1

--- product/media.mk
+++ product/media.mk
@@ -1,4 +1,4 @@
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
