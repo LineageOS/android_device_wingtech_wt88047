b10cfb11fcc1b7214baca8a936ef801fd27fe4b8
Michael Bestas <mkbestas@lineageos.org>
2018-03-19 04:26:16 +0200

    QCamera2: Cleanup makefiles
    Change-Id: I96bd8a79e74e6bed8bcc8d39010411de26ba0fcd

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -1,11 +1,11 @@
-OLD_LOCAL_PATH := $(LOCAL_PATH)
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_CLANG_CFLAGS += \
-        -Wno-error=unused-variable
+    -Wno-error=unused-variable
 
-LOCAL_CFLAGS+= -D_ANDROID_
+LOCAL_CFLAGS += -D_ANDROID_
 LOCAL_CFLAGS += -Wall -Wextra -Werror -Wno-unused-parameter
 
 LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -20,15 +20,10 @@ LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/../../../mm-image-codec/qexif \
     $(LOCAL_PATH)/../../../mm-image-codec/qomx_core
 
-ifeq ($(strip $(TARGET_USES_ION)),true)
-    LOCAL_CFLAGS += -DUSE_ION
-endif
-
-
 ifeq ($(call is-board-platform-in-list, msm8974),true)
-    LOCAL_CFLAGS+= -DMM_JPEG_CONCURRENT_SESSIONS_COUNT=2
+    LOCAL_CFLAGS += -DMM_JPEG_CONCURRENT_SESSIONS_COUNT=2
 else
-    LOCAL_CFLAGS+= -DMM_JPEG_CONCURRENT_SESSIONS_COUNT=1
+    LOCAL_CFLAGS += -DMM_JPEG_CONCURRENT_SESSIONS_COUNT=1
 endif
 
 LOCAL_SRC_FILES := \
@@ -40,11 +35,9 @@ LOCAL_SRC_FILES := \
     src/mm_jpegdec_interface.c \
     src/mm_jpegdec.c
 
-LOCAL_MODULE           := libmmjpeg_interface
+LOCAL_MODULE := libmmjpeg_interface
 LOCAL_32_BIT_ONLY := true
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libqomx_core
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-LOCAL_PATH := $(OLD_LOCAL_PATH)
