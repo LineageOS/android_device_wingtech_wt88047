9e666fadd8d61e2f02589151afcfa0bc545ed787
Michael Bestas <mkbestas@lineageos.org>
2018-03-19 04:26:16 +0200

    QCamera2: Cleanup makefiles
    Change-Id: I96bd8a79e74e6bed8bcc8d39010411de26ba0fcd

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -2,29 +2,29 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_CLANG_CFLAGS += \
-        -Wno-error=unused-private-field \
-        -Wno-error=strlcpy-strlcat-size \
-        -Wno-error=gnu-designator \
-        -Wno-error=unused-variable \
-        -Wno-error=format
+    -Wno-error=unused-private-field \
+    -Wno-error=strlcpy-strlcat-size \
+    -Wno-error=gnu-designator \
+    -Wno-error=unused-variable \
+    -Wno-error=format
 
 LOCAL_SRC_FILES := \
-        QCamera2Factory.cpp \
-        QCamera2Hal.cpp \
-        QCamera2HWI.cpp \
-        QCameraMem.cpp \
-        ../util/QCameraQueue.cpp \
-        ../util/QCameraCmdThread.cpp \
-        QCameraStateMachine.cpp \
-        QCameraChannel.cpp \
-        QCameraStream.cpp \
-        QCameraPostProc.cpp \
-        QCamera2HWICallbacks.cpp \
-        QCameraParameters.cpp \
-        QCameraThermalAdapter.cpp \
-        wrapper/QualcommCamera.cpp
+    QCamera2Factory.cpp \
+    QCamera2Hal.cpp \
+    QCamera2HWI.cpp \
+    QCameraMem.cpp \
+    ../util/QCameraQueue.cpp \
+    ../util/QCameraCmdThread.cpp \
+    QCameraStateMachine.cpp \
+    QCameraChannel.cpp \
+    QCameraStream.cpp \
+    QCameraPostProc.cpp \
+    QCamera2HWICallbacks.cpp \
+    QCameraParameters.cpp \
+    QCameraThermalAdapter.cpp \
+    wrapper/QualcommCamera.cpp
 
-LOCAL_CFLAGS = -Wall -Wextra -Werror
+LOCAL_CFLAGS += -Wall -Wextra -Werror
 
 #use media extension
 #ifeq ($(TARGET_USES_MEDIA_EXTENSIONS), true)
@@ -37,17 +37,18 @@ LOCAL_CFLAGS += -DUSE_MEDIA_EXTENSIONS
 LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON
 
 LOCAL_C_INCLUDES := \
-        $(LOCAL_PATH)/../stack/common \
-        frameworks/native/include/media/openmax \
-        $(call project-path-for,qcom-display)/libgralloc \
-        $(call project-path-for,qcom-display)/libqdutils \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
-        system/media/camera/include \
-        $(LOCAL_PATH)/../../mm-image-codec/qexif \
-        $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
-        $(LOCAL_PATH)/../util \
-        $(LOCAL_PATH)/wrapper \
-        system/media/camera/include
+    $(LOCAL_PATH)/../stack/common \
+    frameworks/native/include/media/openmax \
+    $(call project-path-for,qcom-display)/libgralloc \
+    $(call project-path-for,qcom-display)/libqdutils \
+    $(call project-path-for,qcom-display)/libqservice \
+    $(call project-path-for,qcom-media)/libstagefrighthw \
+    system/media/camera/include \
+    $(LOCAL_PATH)/../../mm-image-codec/qexif \
+    $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
+    $(LOCAL_PATH)/../util \
+    $(LOCAL_PATH)/wrapper \
+    system/media/camera/include
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
@@ -62,7 +63,7 @@ LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libqdMetaDat
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
-LOCAL_MODULE_RELATIVE_PATH    := hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -1,26 +1,18 @@
-OMX_CORE_PATH := $(call my-dir)
-
-# ------------------------------------------------------------------------------
-#                Make the shared library (libqomx_core)
-# ------------------------------------------------------------------------------
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_PATH := $(OMX_CORE_PATH)
+
 LOCAL_MODULE_TAGS := optional
 
-omx_core_defines:= -Werror \
-                   -g -O0
+LOCAL_CFLAGS := -Werror -g -O0
 
-LOCAL_CFLAGS := $(omx_core_defines)
-
-OMX_HEADER_DIR := frameworks/native/include/media/openmax
-
-LOCAL_C_INCLUDES := $(OMX_HEADER_DIR)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../qexif
+LOCAL_C_INCLUDES := \
+    frameworks/native/include/media/openmax \
+    $(LOCAL_PATH)/../qexif
 
 LOCAL_SRC_FILES := qomx_core.c
 
-LOCAL_MODULE           := libqomx_core
+LOCAL_MODULE := libqomx_core
 LOCAL_32_BIT_ONLY := true
 LOCAL_SHARED_LIBRARIES := libcutils libdl
 
