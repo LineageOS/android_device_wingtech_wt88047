d6517aa42bf62d7664b932925be86de07e14d8c6
ghostrider-reborn <adithya.r02@outlook.com>
2018-04-02 10:28:00 +0000

    wt88047: Update audio props for new O audio HAL
    Change-Id: Icb64864b91167762d1eb14aa46c5c1ae42b63f0c

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -17,7 +17,7 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=fluence \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=false
