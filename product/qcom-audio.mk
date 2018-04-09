a6b4a98e2d4fdd51838ac01ee92a92f1f81bafdd
Jason Riordan <jriordan001@gmail.com>
2018-04-09 10:58:11 +0000

    wt88047: Move permissions to vendor
    Change-Id: I210b8772ef05d04b8aa711e92ea623ecbe0191d9

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -13,7 +13,7 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/vendor/etc/permissions/android.hardware.audio.low_latency.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
