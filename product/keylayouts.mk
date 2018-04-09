d0dc2f66033bf6b03c3bee165b7cd870bec857f0
Jason Riordan <jriordan001@gmail.com>
2018-04-09 11:00:27 +0000

    wt88047: Move keylayout files to vendor
    Change-Id: Ief0eb9bc6e2985b714d131ce3021ea205ea603fb

--- product/keylayouts.mk
+++ product/keylayouts.mk
@@ -1,4 +1,4 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/vendor/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/vendor/usr/keylayout/gpio-keys.kl
