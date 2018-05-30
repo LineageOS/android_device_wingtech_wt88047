6ed926e1571ac45bedeee66ac30d58a0ae923a30
Nikolas Lim <nicknitewolf@lineageos.org>
2018-05-31 02:18:41 +0800

    wt88047: Drop shim from mm-qcamera-daemon
    Change-Id: Ic12936c7f36e37489698fad644d1426bd38790af

--- board/shims.mk
+++ board/shims.mk
@@ -1,5 +1,4 @@
 # Shims
 TARGET_LD_SHIM_LIBS += \
-    /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so \
     /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
     /system/vendor/lib/lib-imsvt.so|libshims_ims.so
