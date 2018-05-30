6bafbc6889e0118da77199b2c2fa5d3990490837
Nikolas Lim <nicknitewolf@lineageos.org>
2018-05-31 00:34:47 +0800

    wt88047: Build libwpa_client
    Change-Id: Iaa659120ce451081fa92d83d9594ed21152be5dd

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -3,3 +3,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
     $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/vendor/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
+
+PRODUCT_PACKAGES += \
+    libwpa_client
