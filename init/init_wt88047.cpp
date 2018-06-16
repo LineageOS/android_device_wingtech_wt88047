73f6e72301b9450994730f2d8f7e3529e12a0225
Nikolas Lim <nicknitewolf@lineageos.org>
2018-06-16 14:17:51 +0800

    wt88047: init: Remove device check
    Change-Id: Id5ca48a0e3ceda838e1b3d332cb3253819b43c4c

--- init/init_wt88047.cpp
+++ init/init_wt88047.cpp
@@ -39,14 +39,12 @@
 
 #include <android-base/properties.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
-
+#include "property_service.h"
 #include "util.h"
 
 #include "init_msm8916.h"
 
-using android::base::GetProperty;
 using android::init::property_set;
 
 void property_override(char const prop[], char const value[])
@@ -72,10 +70,6 @@ void init_target_properties()
     std::ifstream fin;
     std::string buf;
     std::string product;
-
-    product = GetProperty("ro.product.name", "");
-    if (product != "wt88047")
-        return;
 
     fin.open("/proc/cmdline");
     while (std::getline(fin, buf, ' '))
