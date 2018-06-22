d8fe29fd5890f8e5d5c8ea29f043a4c5d6a159fc
Nikolas Lim <nicknitewolf@lineageos.org>
2018-06-22 17:39:59 +0800

    wt88047: init: Update dalvik heap values
    Change-Id: Ic4f54d77abea50b8d51433608bcf71ad97155fdb

--- init/init_wt88047.cpp
+++ init/init_wt88047.cpp
@@ -152,6 +152,6 @@ void init_target_properties()
     property_set("dalvik.vm.heapgrowthlimit", is2GB() ? "192m" : "96m");
     property_set("dalvik.vm.heapsize", is2GB() ? "512m" : "256m");
     property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", is2GB() ? "512k" : "2m");
+    property_set("dalvik.vm.heapminfree", "512k");
     property_set("dalvik.vm.heapmaxfree", "8m");
 }
