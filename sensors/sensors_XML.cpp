ff93a7da54b1d4896e054baa1f2935a9f8ea6c56
Nicholas Lim <nicknitewolf@lineageos.org>
2018-08-09 04:08:31 +0800

    wt88047: sensors: Add missing header
    Change-Id: I97d41f9e0bd6d4939c0fd3a8076ea32b38bbab86

--- sensors/sensors_XML.cpp
+++ sensors/sensors_XML.cpp
@@ -32,6 +32,7 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <cutils/log.h>
 #include "unistd.h"
 #include <fcntl.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <private/android_filesystem_config.h>
 
