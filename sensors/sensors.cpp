2231c788843d670342495d62ad6dfaab1876475f
Nicholas Lim <nicknitewolf@lineageos.org>
2018-08-25 01:45:41 +0800

    wt88047: sensors: Fix old style GNU designator extensions
    Change-Id: I94be87368618cb65c655ab98b547290e65335758

--- sensors/sensors.cpp
+++ sensors/sensors.cpp
@@ -54,20 +54,20 @@ static int sensors__get_sensors_list(struct sensors_module_t*,
 }
 
 static struct hw_module_methods_t sensors_module_methods = {
-		open: open_sensors
+		.open = open_sensors
 };
 
 struct sensors_module_t HAL_MODULE_INFO_SYM = {
-		common: {
-				tag: HARDWARE_MODULE_TAG,
-				version_major: 1,
-				version_minor: 0,
-				id: SENSORS_HARDWARE_MODULE_ID,
-				name: "Quic Sensor module",
-				author: "Quic",
-				methods: &sensors_module_methods,
-				dso: NULL,
-				reserved: {0},
+		.common = {
+				.tag = HARDWARE_MODULE_TAG,
+				.version_major = 1,
+				.version_minor = 0,
+				.id = SENSORS_HARDWARE_MODULE_ID,
+				.name = "Quic Sensor module",
+				.author = "Quic",
+				.methods = &sensors_module_methods,
+				.dso = NULL,
+				.reserved = {0},
 		},
 		get_sensors_list: sensors__get_sensors_list,
 };
