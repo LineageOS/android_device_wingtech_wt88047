0f11794657f6c9d673c70221bfa379ba469e2557
Kevin F. Haggerty <haggertk@lineageos.org>
2018-09-20 22:46:44 -0600

    wt88047: sensors: Update multihal with P changes
    Change-Id: Iee33e9043e5f4eb9b5ff8af562800e0392c041b5

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -74,7 +74,7 @@ LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_VENDOR_MODULE := true
 
-LOCAL_CFLAGS := -DLOG_TAG=\"MultiHal\"
+LOCAL_CFLAGS := -Wall -Werror -DLOG_TAG=\"MultiHal\"
 
 LOCAL_SRC_FILES := \
     multihal.cpp \
