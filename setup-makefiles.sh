1aa447f0688716368270facf287aaf32d14159a7
Michael Bestas <mkbestas@lineageos.org>
2018-12-30 15:45:12 +0000

    wt88047: TWRP-ify
    Change-Id: Id94c9fa24e15a68c3e14e58b57cec5da92ed003f
    Signed-off-by: Ketut P. Kumajaya <ketut.kumajaya@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -44,5 +44,12 @@ write_headers
 
 write_makefiles "$MY_DIR"/proprietary-files.txt
 
+# Blobs for TWRP data decryption
+cat << EOF >> "$BOARDMK"
+ifeq (\$(WITH_TWRP),true)
+TARGET_RECOVERY_DEVICE_DIRS += vendor/$VENDOR/$DEVICE/proprietary
+endif
+EOF
+
 # Finish
 write_footers
