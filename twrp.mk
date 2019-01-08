166695c7226859be1894d962e72d6f024dbc709f
Ketut P. Kumajaya <ketut.kumajaya@gmail.com>
2019-01-08 15:43:27 +0000

    wt88047: twrp: Use system's vold for decryption
    Change-Id: I207c61b8730879bca6ccefd7174437ad0adf6f9a
    Signed-off-by: Ketut P. Kumajaya <ketut.kumajaya@gmail.com>

--- twrp.mk
+++ twrp.mk
@@ -1,3 +1,4 @@
 TARGET_RECOVERY_DEVICE_DIRS += device/wingtech/wt88047/twrp
 TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd hwservicemanager keymaster-3-0
 TW_THEME := portrait_hdpi
