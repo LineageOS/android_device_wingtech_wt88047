4a6b4a48097edcbf90ee72e784ad466b6aea882d
GuaiYiHu <guaiyihu@foxmail.com>
2019-06-09 16:23:01 +0800

    wt88047: Add keydisabler support
    * Credit to BadDaemon for figuring out why my
      file ownership and sepolicy was wrong.
    
    Change-Id: I5835947c5356f5b79d6b2f7ddcf67b303a09417e

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.wt88047
+
 # USB ID
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.usb.vid=2717 \
