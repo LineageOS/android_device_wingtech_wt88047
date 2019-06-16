de17065d723b0a2b6a33cb41241607274b8ded82
Nolen Johnson <johnsonnolen@gmail.com>
2019-06-16 05:12:49 +0000

    wt88047: system-properties: Migrate from system.prop to system_prop.mk
    * The build system auto-including system.prop will be deprecated in the
      near future, so migrate to setting all system propeties with a makefile.
    
    Change-Id: Ia640d4a8adf2eaa68c801aba084fd01063af1463
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Signed-off-by: Nicholas Lim <nicknitewolf@lineageos.org>

---
+++ system_prop.mk
@@ -0,0 +1,34 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=false
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cpp.duplication=false \
+    persist.camera.hal.debug.mask=0 \
+    ro.camera.sensors=ov8865_q8v18a,ov2680_skuhf,ov2680_5987fhq
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320
+
+# Factory Reset Protection
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/platform/soc.0/7824900.sdhci/by-name/config
+
+# Fusion mag field filter
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fusion.magfield.max=250
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.target=dpm1 \
+    persist.radio.multisim.config=dsds \
+    persist.radio.custom_ecc=1 \
+    persist.radio.ecc_hard_1=112,911,110,122,119,120,000,118 \
+    persist.radio.ecc_hard_count=1 \
+    rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
+    ril.subscription.types=RUIM
